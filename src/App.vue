95298d715c1446f9efd0e8e010763400cce73701
lufk <zhanjiangmrlu@163.com>
2018-02-06 16:56:03 +0800

    Tab栏Table表格完成

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
     <left-Nav></left-Nav>
-    <router-view/>
+
+    <div class="main">
+      <router-view/>
+    </div>
   </div>
 </template>
 
@@ -10,21 +13,28 @@
 import leftNav from "@/components/common/leftNav";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     leftNav
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
   height: 100%;
+}
+
+.main {
+  float: left;
+  height: 100%;
+  overflow: auto;
+  width: 96%;
 }
 </style>
--- src/components/page/Pos.vue
+++ src/components/page/Pos.vue
@@ -1,10 +1,44 @@
 <template>
   <div class="pos">
-    <el-tabs v-model="activeName" @tab-click="handleClick">
-      <el-tab-pane label="点餐" name="first">点餐</el-tab-pane>
-      <el-tab-pane label="挂单" name="second">挂单</el-tab-pane>
-      <el-tab-pane label="外卖" name="third">外卖</el-tab-pane>
-    </el-tabs>
+    <el-row>
+      <el-col :span="7">
+        <el-tabs v-model="activeName" @tab-click="handleClick">
+          <el-tab-pane label="点餐" name="first" class="dcan">
+            <el-table :data="tableData3" border>
+              <el-table-column prop="goodName" label="商品名称" width="110">
+              </el-table-column>
+              <el-table-column prop="count" label="数量" width="60">
+              </el-table-column>
+              <el-table-column prop="money" label="金额" width="110">
+              </el-table-column>
+              <el-table-column prop="operation" label="操作" width="110">
+                <template scope="scope">
+                  <el-button type="text" size="small">删除</el-button>
+                  <el-button type="text" size="small">增加</el-button>
+                </template>
+              </el-table-column>
+            </el-table>
+          </el-tab-pane>
+          <el-tab-pane label="挂单" name="second">
+            挂单
+          </el-tab-pane>
+          <el-tab-pane label="外卖" name="third">
+            外卖
+          </el-tab-pane>
+        </el-tabs>
+
+        <div class="btn">
+          <el-button type="warning" size="small">挂单</el-button>
+          <el-button type="danger" size="small">删除</el-button>
+          <el-button type="success" size="small">结账</el-button>
+        </div>
+
+      </el-col>
+
+      <el-col :span="17">
+        111
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -13,7 +47,33 @@ export default {
   name: "Pos",
   data() {
     return {
-      activeName: 'second'
+      activeName: "second",
+      tableData3: [
+        {
+          goodName: "可口可乐",
+          count: "1",
+          money: "5",
+          operation: ""
+        },
+        {
+          goodName: "香辣鸡腿堡",
+          count: "1",
+          money: "8",
+          operation: ""
+        },
+        {
+          goodName: "爱心薯条",
+          count: "1",
+          money: "10",
+          operation: ""
+        },
+        {
+          goodName: "甜筒",
+          count: "1",
+          money: "10",
+          operation: ""
+        }
+      ]
     };
   },
   methods: {
@@ -23,4 +83,26 @@ export default {
   }
 };
 </script>
-s
+<style lang="less">
+.el-tabs__item {
+  text-indent: 20px;
+}
+
+.el-table .cell {
+  text-align: center;
+}
+
+.has-gutter {
+  background-color: #ccc;
+}
+
+.btn {
+  padding-top: 15px;
+}
+
+// .grid-content {
+//   height: 100%;
+// }
+</style>
+
+
